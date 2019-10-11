b6de6b843b8d9522db1dd0afad88856eabdc5ecf
sevJohnston <severena@comcast.net>
2019-10-11 10:51:55 -0700

    Lab 1 completed

--- OregonHikes/Controllers/HomeController.cs
+++ OregonHikes/Controllers/HomeController.cs
@@ -10,30 +10,60 @@ namespace OregonHikes.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index()    //view for the home page
         {
             return View();
         }
 
-        public IActionResult About()
+        public IActionResult About()    //view for the about/history page
         {
-            ViewData["Message"] = "Your application description page.";
+            return View("About");
+        }
 
+        [HttpGet]
+        public ViewResult ReviewForm()     //default constructor
+        {
+            return View("ReviewForm");
+        }
+        //overloaded constructor
+        [HttpPost]
+        public ViewResult ReviewForm(UserReviews userReviews)   //adds a review to the list, thanks the user
+        {
+            Repository.AddResponse(userReviews);
+            return View("Submitted", userReviews);
+        }
+
+        public ViewResult HikeReviews()     
+        {
+            return View(Repository.reviews);
+        }
+
+        public IActionResult Contact()      //view for the contact page
+        {
+            return View("Contact");
+        }
+
+        public ViewResult Resources()       //view for the resources page
+        {
+            return View("Resources");
+        }
+
+        public ViewResult Books()       //view for the print resources page
+        {
+            return View("Books");
+        }
+
+        public ViewResult Links()       //view for the online resources pages
+        {
+            return View("Links");
+        }
+
+        public IActionResult Privacy()      //view for the default privacy page
+        {
             return View();
         }
 
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+        //not sure what this method does-- default method
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ OregonHikes/Models/Repository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OregonHikes.Models
+{
+    public class Repository
+    {
+        public static List<UserReviews> reviews = new List<UserReviews>();      //creates a list for userReviews
+
+        public static IEnumerable<UserReviews> Reviews
+        {
+            get
+            {
+                return reviews;
+            }
+        }
+        
+        public static void AddResponse(UserReviews userReviews) //method adds review to the reviews list
+        {
+            reviews.Add(userReviews);
+        }
+            
+    
+    }
+}
